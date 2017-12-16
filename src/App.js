1aa26189f7dc2b7a686abb38ad543ab9a8814c62
Wojciech <wojciech.paluch@o2.pl>
2017-12-16 11:36:10 +0100

    added get,post method

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class App extends Component {
 
-  getHandler = () => {
+    state = {response: 'No response yet!'}
 
-    fetch('https://ad-snadbox.firebaseio.com/users/Wojciech.json').then(response => response.json())
-        .then(console.log)
-  }
+    getHandler = () => {
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.getHandler}>Get Data from our endpoint</button>
-      </div>
-    );
-  }
-}
+        fetch('https://ad-snadbox.firebaseio.com/users/Wojciech.json').then(response => response.json())
+            .then((json) => {
+                this.setState({
+                    response: JSON.stringify(json)
+                })
+            })
+    }
 
-export default App;
+    postHandler = () => {
+
+        fetch ('https://ad-snadbox.firebaseio.com/users/Wojciech.json',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+
+                    name: "Wojciech",
+                    lastname: "Paluch",
+                    active: false
+
+                })
+
+            }
+        )
+    }
+
+        render()
+        {
+            return (
+                <div>
+                    <button onClick={this.getHandler}>Get Data from our endpoint</button>
+
+                    <p>
+                        {this.state.response}
+                    </p>
+
+                    <button onClick={this.postHandler}>Post Data from our endpoint</button>
+                </div>
+            );
+        }
+    }
+
+    export
+    default
+    App;
